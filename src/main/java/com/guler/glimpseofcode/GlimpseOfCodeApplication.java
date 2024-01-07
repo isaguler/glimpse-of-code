b9693cb65e97ef8e0107d61b309ea6b1428708fc
Gülers <jesus3419@gmail.com>
2024-01-07 14:54:29 +0300

    Init <-_->

---
+++ src/main/java/com/guler/glimpseofcode/GlimpseOfCodeApplication.java
@@ -0,0 +1,13 @@
+package com.guler.glimpseofcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlimpseOfCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlimpseOfCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guler/glimpseofcode/GlimpseOfCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guler.glimpseofcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlimpseOfCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
