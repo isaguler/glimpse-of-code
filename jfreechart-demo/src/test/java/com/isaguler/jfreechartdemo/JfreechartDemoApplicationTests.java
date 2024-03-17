79272f9b6ed1c521a45a28645cfc1cf93645b1ed
Gülers <jesus3419@gmail.com>
2024-03-17 13:38:59 +0300

    JFreeChart Demo

---
+++ jfreechart-demo/src/test/java/com/isaguler/jfreechartdemo/JfreechartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaguler.jfreechartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfreechartDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
