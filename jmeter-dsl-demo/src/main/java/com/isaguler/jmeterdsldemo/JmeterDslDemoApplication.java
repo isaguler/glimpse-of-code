c057ef6d9fc2be589f4df3857a21760fe2b16858
Gülers <jesus3419@gmail.com>
2024-05-12 14:08:31 +0300

    Jmeter Dsl Demo Part-1

---
+++ jmeter-dsl-demo/src/main/java/com/isaguler/jmeterdsldemo/JmeterDslDemoApplication.java
@@ -0,0 +1,13 @@
+package com.isaguler.jmeterdsldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterDslDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterDslDemoApplication.class, args);
+	}
+
+}
