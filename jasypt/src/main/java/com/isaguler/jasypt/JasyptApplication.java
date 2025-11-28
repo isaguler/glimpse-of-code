c252a2e1f4c283c957b1ec8ac2048686d4e5b6b5
Gülers <jesus3419@gmail.com>
2025-11-28 13:12:27 +0300

    jasypt

---
+++ jasypt/src/main/java/com/isaguler/jasypt/JasyptApplication.java
@@ -0,0 +1,15 @@
+package com.isaguler.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEncryptableProperties
+public class JasyptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasyptApplication.class, args);
+	}
+
+}
